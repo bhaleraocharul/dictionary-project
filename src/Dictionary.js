2abad2d873c62ee151dcc5f87e8875db22ebaa53
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-25 17:44:17 +1000

    integrated the part for Synonyms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
-import Results from "./Results";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState(" ");
+  let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
@@ -14,9 +14,8 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
-    //documentation: https://dictionaryapi.dev/
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -27,9 +26,8 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <input type="search" onChange={handleKeywordChange} />
       </form>
-
       <Results results={results} />
     </div>
   );
