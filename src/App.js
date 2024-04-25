9e6f3cd381cb0dc1f352e22375328a2166e8284b
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-25 14:54:51 +1000

    search input functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Dictionary App</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Charul</small>
+        </footer>
+      </div>
     </div>
   );
 }
