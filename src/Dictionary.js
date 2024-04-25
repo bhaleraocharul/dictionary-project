814bf9407f491510ee11848b10b2630d80c5c7b2
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-25 16:41:48 +1000

    added the meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    // documentation: https://www.shecodes.io/learn/apis/dictionary
-    let apiKey = "1o630f4a9d3497230622bb9ffta16859";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(apiUrl).then(handleResponse);
   }
@@ -28,6 +29,8 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+
+      <Results results={results} />
     </div>
   );
 }
