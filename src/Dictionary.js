37f22fa0942bb300d25eae1963c08d798b41aeb6
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-04-26 12:01:40 +1000

    pexels api integrated but giving 401

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,24 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
   }
 
   function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "ToI7qqgzgJu29TbSzviwpsFTLwixrdnDjJpDUxpmRtMy1OohXWCOfRLS";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
